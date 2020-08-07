e21c3a2a308f9b6d46c939561f2a73b5c08a369d
amesrobotics <ames.amperes@gmail.com>
2020-08-06 20:02:34 -0600

    testing if new branch was a success.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,7 +56,7 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
-
+// Testing new branch
   /**
    * Use this method to define your button->command mappings.  Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
